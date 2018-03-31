06553ef4967166e16681b4c9158a2651fb3696fc
3dsoundfromtagan <ostapiv.alex@gmail.com>
2018-03-31 22:24:50 +0300

    test2

--- SFML_test/cmake-build-debug/CMakeFiles/SFML_test.dir/DependInfo.cmake
+++ SFML_test/cmake-build-debug/CMakeFiles/SFML_test.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/alex/CLionProjects/SFML_test/main.cpp" "/home/alex/CLionProjects/SFML_test/cmake-build-debug/CMakeFiles/SFML_test.dir/main.cpp.o"
+  "/home/alex/CLionProjects/game/SFML_test/main.cpp" "/home/alex/CLionProjects/game/SFML_test/cmake-build-debug/CMakeFiles/SFML_test.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- SFML_test/cmake-build-debug/cmake_install.cmake
+++ SFML_test/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/alex/CLionProjects/SFML_test
+# Install script for directory: /home/alex/CLionProjects/game/SFML_test
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/alex/CLionProjects/SFML_test/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/alex/CLionProjects/game/SFML_test/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
