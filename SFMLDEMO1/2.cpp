fab806e1fd9c42bc17b42a971fbb17b3746de77d
3dsoundfromtagan <ostapiv.alex@gmail.com>
2018-04-03 01:59:17 +0300

    not fixed

--- SFMLDEMO1/2.cpp
+++ SFMLDEMO1/2.cpp
@@ -3,44 +3,47 @@
 #include <time.h>
 #include <cmath>
 using namespace sf;
-bool isMove = false,df = true;
+bool isMove = false, df = true;
 bool fly = false;
-float dx,x=100,y=100;
+float dx, x = 100, y = 100;
 float dy;
 CircleShape cirkle(40);
-RenderWindow window(VideoMode(640, 480), "Hello SFML");
+RenderWindow window(VideoMode(640, 480), "Our beautiful game");
 CircleShape circle(30);
 void flyr()
 {
-    float start_x = cirkle.getPosition().x,start_y= cirkle.getPosition().y;
+    double start_x = cirkle.getPosition().x, start_y= cirkle.getPosition().y;
+
     Clock clock;
     clock.restart();
-    float al = atan((100-start_y)/(100-start_x)) ;
-    float speed_x = 2*sqrt((100 - start_x)*(100-start_x)+(100-start_y)*(100-start_y))*cos(al);
-    float speed_y = 2*sqrt((100 - start_x)*(100-start_x)+(100-start_y)*(100-start_y))*sin(al);
-    float speed_y1;
-    while((sqrt(speed_x*speed_x+speed_y*speed_y) >10))
+
+    double al = atan((100 - start_y) / (100 - start_x)) ;
+    double speed_x = 2 * sqrt((100 - start_x) * (100-start_x) + (100 - start_y) * (100 - start_y)) * cos(al);
+    double speed_y = 2 * sqrt((100 - start_x) * (100-start_x) + (100 - start_y) * (100 - start_y)) * sin(al);
+    double speed_y1;
+
+    while((sqrt(speed_x * speed_x + speed_y * speed_y) > 10))
     {
         float time = clock.getElapsedTime().asSeconds();
-        speed_y1 =speed_y+ 50*time;
-        cirkle.setPosition(start_x+speed_x*time,start_y+speed_y*time+50*time*time);
+        speed_y1 = speed_y + 200 * time;
+        cirkle.setPosition((float)(start_x + speed_x * time), (float)(start_y + speed_y * time + 100 * time * time));
         if(circle.getGlobalBounds().intersects(cirkle.getGlobalBounds()))
             circle.setPosition(-100,0);
-        if((cirkle.getPosition().x>=560))
+        if((cirkle.getPosition().x >= 550))
         {
             start_x = cirkle.getPosition().x;
             start_y = cirkle.getPosition().y;
-            speed_x = -0.5*speed_x;
-            speed_y1 = -0.5*speed_y1;
+            speed_x = -0.9 * speed_x;
+            speed_y1 = -0.9 * speed_y1;
             speed_y = speed_y1;
             clock.restart();
         }
-        if((cirkle.getPosition().y>=400))
+        if((cirkle.getPosition().y >= 410))
         {
             start_x = cirkle.getPosition().x;
             start_y = cirkle.getPosition().y;
-            speed_x = 0.5*speed_x;
-            speed_y1 = -0.5*speed_y1;
+            speed_x = 0.9 * speed_x;
+            speed_y1 = -0.9 * speed_y1;
             speed_y = speed_y1;
             clock.restart();
         }
@@ -59,7 +62,7 @@ int main() {
 
 
 
-    cirkle.setPointCount(3);
+    cirkle.setPointCount(30);
     cirkle.setFillColor(Color::Green);
     cirkle.setPosition(100,100);
     circle.setPointCount(13);
